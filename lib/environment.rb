f181a7c28815041038df267051f649e7fa222192
angelo milien <angelomilienjoseph@gmail.com>
2021-03-29 18:10:06 -0400

    made changs on files location

--- lib/environment.rb
+++
@@ -1,9 +0,0 @@
-require "awesome_print"
-require "httparty"
-require "pry"
-require "dotenv/load"
-
-require_relative "./weather/version"
-require_relative "./weather/api"
-require_relative "./weather/weather"
-require_relative "./weather/cli"
---
+++ lib/weather/environment.rb
@@ -0,0 +1,9 @@
+require "awesome_print"
+require "httparty"
+require "pry"
+require "dotenv/load"
+
+require_relative "./version"
+require_relative "./api"
+require_relative "../weather"
+require_relative "./cli"
