f72f3775cf9cd904e353b202fd2d1148cfea8044
angelo milien <angelomilienjoseph@gmail.com>
2021-03-31 20:51:52 -0400

    updated

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -19,7 +19,7 @@ class Weather::Cli
     def get_zip_code_from_user
         puts "Enter your zipcode"
         input_3 = gets.strip 
-        display_weather(Weather::CurrentWeather.find_or_create_by_zip_code(input_3))
+        Weather::CurrentWeather.find_or_create_by_zip_code(input_3)
     end
 
     # ZIPCODE AND STATE COMMON FUNCTIONS
@@ -53,6 +53,33 @@ class Weather::Cli
         @main_while_is = false
     end
 
+    def get_fourth_nested(current_weather)
+        case current_weather.message # if weather is found or created it's message will be yes 
+            when "yes"
+                first_display(current_weather)
+
+                while @fourth_nested
+                    input_4 = gets.strip
+                    case input_4.upcase
+                        when "YES", "Y"
+                           display_weather(current_weather)
+                           puts "program is terminated thank you!"
+                           terminate_program_on_fourth_deep_layer  # will continue with more code after
+                           break
+                        when "NO","N"
+                           puts "program is terminated thank you!"
+                           terminate_program_on_fourth_deep_layer
+                           break
+                        else
+                           puts ""
+                           puts "Wrong input please try again"
+                    end
+                end
+            else
+                puts current_weather.message #You entered a wrong entry please try again!
+        end
+    end
+
     def greeting_user
 
         @main_while_is = true
@@ -70,33 +97,6 @@ class Weather::Cli
                 @first_nested = true
             end
 
-            def get_fourth_nested(current_weather)
-                case current_weather.message # if weather is found or created it's message will be yes 
-                    when "yes"
-                        first_display(current_weather)
-                        #  get_fourth_nested
-                        while @fourth_nested
-                            input_4 = gets.strip
-                            case input_4.upcase
-                                when "YES", "Y"
-                                   display_weather(current_weather)
-                                   puts "program is terminated thank you!"
-                                   terminate_program_on_fourth_deep_layer  # will continue with more code after
-                                   break
-                                when "NO","N"
-                                   puts "program is terminated thank you!"
-                                   terminate_program_on_fourth_deep_layer
-                                   break
-                                else
-                                   puts ""
-                                   puts "Wrong input please try again"
-                            end
-                        end
-                    else
-                        puts current_weather.message #You entered a wrong entry please try again!
-                end
-            end
-
             while @first_nested
                 input_1 = gets.strip
                 case input_1.upcase
@@ -110,41 +110,21 @@ class Weather::Cli
 
                                     while @third_nested
                                         current_weather = get_state_name_from_user
-                                        # case current_weather.message # if weather is found or created it's message will be yes 
-                                        #     when "yes"
-                                        #         first_display(current_weather)
-                                                 get_fourth_nested(current_weather)
-                                        #         while @fourth_nested
-                                        #             input_4 = gets.strip
-                                        #             case input_4.upcase
-                                        #                 when "YES", "Y"
-                                        #                    display_weather(current_weather)
-                                        #                    puts "program is terminated thank you!"
-                                        #                    terminate_program_on_fourth_deep_layer  # will continue with more code after
-                                        #                    break
-                                        #                 when "NO","N"
-                                        #                    puts "program is terminated thank you!"
-                                        #                    terminate_program_on_fourth_deep_layer
-                                        #                    break
-                                        #                 else
-                                        #                    puts ""
-                                        #                    puts "Wrong input please try again"
-                                        #             end
-                                        #         end
-                                        #     else
-                                        #        puts current_weather.message #You entered a wrong entry please try again!
-                                        # end
+                                        get_fourth_nested(current_weather)        
                                     end
-                                when "zipcode","z" # second_nested scope
-                                    puts "Enter your zipcode"
-                                    current_weather = get_zip_code_from_user
+                                when "zipcode","z" 
+                                   
+                                    while @third_nested
+                                        current_weather = get_zip_code_from_user
+                                        get_fourth_nested(current_weather)
+                                    end
                                 else
                                     puts ""
                                     puts "Please enter a valid input"
                             end
                         end
             
-                    when "NO","N"  # first_nested scope
+                    when "NO","N" 
                         puts "program is terminated thank you!"
                         @main_while_is = false
                         break
@@ -155,16 +135,9 @@ class Weather::Cli
             end
         end
     end
-
-    # Iteration Methods Section
-            
-    def ask_for_input_2
-        puts "for state enter 'state/s' and 'zipcode/z' for zipcode"
-        input_2 = gets.strip
-    end
 end
 
-#Notes
+# my Notes
 
 # description = case 16
 # when 13..19 then "teenager"
